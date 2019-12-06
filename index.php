55cce78d10b24496ce90bde6aa6799fd29fcb309
sayfatay <sayfatay1234@gmail.com>
2019-12-06 17:06:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Hello LINE BOT";
+require "vendor/autoload.php";
+$access_token = 'UHuhOvtaKhe9FpKkcqOLL132zDsTy84bhuC3JN1BiIdZhw06CFYTv+YBxRwmSZnRzR0dGpUIy3FQkTqJnylG7Z6udY0FXLRWpWXRDByjdt7sABwhge/C2Kbd00zHsV9VP3AfQsSMnciD5QAEyM/WgAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '8df2c5e814e2234260110eb479dd68fe';
+$idPush = '1653628392'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
